a39cd543c69d221ad3c0fb9759229e35ac2289c8
Ricardo <ricardofps66@gmail.com>
2019-03-21 17:51:54 -0300

    Finalizado

--- Java/TRABALHO_JAVA/Capitulo_05/Ex05/Ret.java
+++ Java/TRABALHO_JAVA/Capitulo_05/Ex05/Ret.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Ret{
   Scanner input= new Scanner(System.in);
   Random random = new Random();
-  int ocupado = 1, desocupado = 0,n=1;
+  int ocupado = 1, desocupado = 0,n=1,andar,quarto;
   void retorna(){
     int vet[][] = new int[5][10];
     for(int i = 0 ; i < vet.length ; i++){
@@ -11,22 +11,37 @@ public class Ret{
         vet[i][j] = random.nextInt(2);
       }
     }
-    System.out.println("[1]- Listar quarto\n[2]- Definir ocupação\n[3]- Sair");
-    n = input.nextInt();
-    switch(n){
-      case 1:
-        for(int i = 0 ; i < vet.length ; i++){
-          System.out.println(i+"° Andar");
-          for(int j = 0 ; j < vet.length ; j++){
-            if(vet[i][j] == 0){
+    do{
+        System.out.println("Você deseja:\n[1]- Listar quarto\n[2]- Definir ocupação\n[3]- Sair");
+        n = input.nextInt();
+        switch(n){
+          case 1:
+            for(int i = 0 ; i < vet.length ; i++){
+              System.out.println((i+1)+"° Andar");
+              for(int j = 0 ; j < 10 ; j++){
+                if(vet[i][j] == 0){
 
-              System.out.println("Quarto Número: "+vet[i][j]+" Desocupado");
-            }else{
-              System.out.println("Quarto Número: "+vet[i][j]+" Ocupado");
+                  System.out.println("Quarto Número: "+(j+1)+" Desocupado");
+                }else{
+                  System.out.println("Quarto Número: "+(j+1)+" Ocupado");
+                }
+              }
             }
-          }
-        }
-    }
+            break;
+            case 2:
+                System.out.println("Deseja alterar o estado de qual andar ? ");
+                andar = input.nextInt();
+                System.out.println("Deseja alterar o estado de qual quarto ? ");
+                quarto = input.nextInt();
+                if(vet[andar-1][quarto-1]==0){
+                  vet[andar-1][quarto-1]=1;
+                }else{
+                  vet[andar-1][quarto-1]=0;
+                }
+                break;
 
-  }
+            }
+
+          }while(n!=3);
+      }
 }
